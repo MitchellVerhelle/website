5f0e114d3c694d4561d63dd5c3a614ad1438e486
MitchellVerhelle <mitch@verhelle.com>
2025-06-03 16:08:57 -0500

    Project names

--- src/lib/projects.ts
+++ src/lib/projects.ts
@@ -6,6 +6,13 @@ export interface Project {
 }
 
 export const projects: Project[] = [
-  { slug: 'awesome-project', title: 'Awesome Project', videoUrl: 'https://…' },
-  { slug: 'cool-app', title: 'Cool App', imageUrl: '/img/cool-app.png' },
+  { slug: 'dream-catcher', title: 'Dream Catcher', videoUrl: 'https://…' },
+  { slug: 'dex-tokenomics', title: 'DEX Tokenomics Simulation', videoUrl: 'https://…' },
+  { slug: 'riscv-emulator', title: 'RISC-V Emulator', imageUrl: '/img/cool-app.png' },
+  { slug: 'gpu', title: 'GPU Option Pricer', imageUrl: '/img/cool-app.png' },
+  { slug: 'extreme-values', title: 'Extreme Value Distributions', imageUrl: '/img/cool-app.png' },
+  { slug: 'mcm', title: 'MCM 2024', imageUrl: '/img/cool-app.png' },
+  { slug: 'cmcm', title: 'CMCM 2023', imageUrl: '/img/cool-app.png' },
+  { slug: 'sport-bet', title: 'BetScout Sports Betting System', imageUrl: '/img/cool-app.png' },
+  { slug: 'kaggle', title: 'Kaggle', imageUrl: '/img/cool-app.png' },
 ];
